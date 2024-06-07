51d8b08a04f7eedb519e08fde2cb40cc58477ce0
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-06-07 15:10:29 +0530

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Library</a>
+    <Link class="navbar-brand" to="#">Library</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Add Book</a>
+          <Link class="nav-link active" aria-current="page" to="/">Add Book</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/search">Search Book</a>
+          <Link class="nav-link" to="/search">Search Book</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/delete">Delete Book</a>
+          <Link class="nav-link" to="/delete">Delete Book</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/viewall">Book Details</a>
+          <Link class="nav-link" to="/viewall">Book Details</Link>
         </li>
       </ul>
     </div>
