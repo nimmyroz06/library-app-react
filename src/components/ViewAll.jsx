893e386c46efdeea28562b82f9b544f4ae2f21ee
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-06-07 14:11:12 +0530

    Navbar  added

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const ViewAll = () => {
   return (
     <div>
+        <NavBar/>
         <center><h1><b>Book Details</b></h1></center><br></br>
         <table class="table">
   <thead>
