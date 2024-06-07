27a232efc4dce5a4e1cf2821d4bc8e83632652f0
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-06-07 13:03:19 +0530

    Search added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBook from './components/AddBook';
+import SearchBook from './components/SearchBook';
 
 function App() {
   return (
     <div>
       <AddBook/>
+      <SearchBook/>
     </div>
   );
 }
