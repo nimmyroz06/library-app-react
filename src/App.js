899ac5713c001e6ff6d749d32a984cbf09da583c
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-06-07 13:43:42 +0530

    Delete added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddBook from './components/AddBook';
 import SearchBook from './components/SearchBook';
+import DeleteBook from './components/DeleteBook';
 
 function App() {
   return (
     <div>
       <AddBook/>
       <SearchBook/>
+      <DeleteBook/>
     </div>
   );
 }
--- src/components/SearchBook.jsx
+++ src/components/SearchBook.jsx
@@ -8,7 +8,7 @@ const SearchBook = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <label htmlFor="" className="form-label">Book Titile</label>
+                            <label htmlFor="" className="form-label">Book Title</label>
                             <input type="text" className="form-control" />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
